fe61dddcb4237b555e4185899aa8c1e4c71fa613
IgorChernyshov <ichernyshov@bk.ru>
2021-12-13 19:47:07 +0300

    UnitsConvertor - Initial Commit

---
+++ UnitsConvertor/UnitsConvertor/UnitsConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitsConvertorApp.swift
+//  UnitsConvertor
+//
+//  Created by Igor Chernyshov on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct UnitsConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

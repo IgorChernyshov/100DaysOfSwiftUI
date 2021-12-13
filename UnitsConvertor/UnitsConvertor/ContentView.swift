9c2037f06f475cf5f1f473480b44d0f47356e55c
IgorChernyshov <ichernyshov@bk.ru>
2021-12-13 21:19:34 +0300

    Units converter finished basic version

--- UnitsConvertor/UnitsConvertor/ContentView.swift
+++ UnitsConvertor/UnitsConvertor/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+
+	// MARK: - Temperature
+	@State private var fromTemperature: String = "36.6"
+	@State private var fromTemperatureUnits: UnitTemperature = .celsius
+	@State private var toTemperatureUnits: UnitTemperature = .fahrenheit
+
+	private var toTemperature: String {
+		guard let fromTemperatureAsDouble = Double(fromTemperature) else { return "🤷🏻‍♂️" }
+		let measurement = Measurement(value: fromTemperatureAsDouble, unit: fromTemperatureUnits)
+		let convertedMeasurement = measurement.converted(to: toTemperatureUnits)
+		let displayedMeasurement = convertedMeasurement.value.rounded(.toNearestOrEven).description
+		return displayedMeasurement + " " + convertedMeasurement.unit.symbol
+	}
+
+	private let displayedTemperatureUnits: [UnitTemperature] = [.fahrenheit, .celsius, .kelvin]
+
+	var body: some View {
+		NavigationView {
+			Form {
+				Section {
+					TextField("Convert from", text: $fromTemperature, prompt: nil)
+						.keyboardType(.decimalPad)
+					Picker("From units", selection: $fromTemperatureUnits) {
+						ForEach(displayedTemperatureUnits, id: \.self) {
+							Text($0.symbol)
+						}
+					}
+					Picker("To units", selection: $toTemperatureUnits) {
+						ForEach(displayedTemperatureUnits, id: \.self) {
+							Text($0.symbol)
+						}
+					}
+					Text(toTemperature)
+				} header: {
+					Text("Temperature")
+				}
+			}
+			.navigationTitle("Units Converter")
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }

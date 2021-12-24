d147be226181c392b1d3a1c7d3bed524d6495a02
IgorChernyshov <ichernyshov@bk.ru>
2021-12-24 17:56:06 +0300

    GuessTheFlag - Initial Commit

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Igor Chernyshov on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

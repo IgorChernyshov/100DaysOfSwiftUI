4b8ca34ac31718a52491e636e62088137c8ab3c4
IgorChernyshov <ichernyshov@bk.ru>
2021-12-26 13:18:51 +0300

    ViewsAndModifiers - Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Igor Chernyshov on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

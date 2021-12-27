84eda5f83c5a1b0f7432ad4fd045f9d2be825385
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-12-27 15:46:58 +0300

    Completed guided part

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		VStack {
+			Text("Gryffindor")
+				.font(.largeTitle)
+				.blur(radius: 1)
+			Text("Hufflepuff")
+			Text("Ravenclaw")
+			Text("Slytherin")
+
+			Text("Hello, world!")
+				.padding()
+				.background(.red)
+				.padding()
+				.background(.blue)
+				.padding()
+				.background(.green)
+				.padding()
+				.background(.yellow)
+		}
+		// Modifiers like fonts are applied to all child views
+		// They are overriden by child individual modifiers
+		.font(.body)
+		// Modifiers like blur are applied to all child views
+		// Any individual child modifiers are then added to parent modifier
+		.blur(radius: 1)
     }
 }
 

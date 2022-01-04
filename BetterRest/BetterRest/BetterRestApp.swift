9226ba2f76b48e5852291b998de252e9057cd01d
IgorChernyshov <ichernyshov@bk.ru>
2022-01-04 10:33:54 +0300

    BetterRest - finished guided part

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Igor Chernyshov on 01.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

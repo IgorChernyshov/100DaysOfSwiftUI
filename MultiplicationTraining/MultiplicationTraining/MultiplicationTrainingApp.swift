bfaf6cadbbf561b83e2efeebbd5efbad714739c2
IgorChernyshov <ichernyshov@bk.ru>
2022-01-09 17:25:13 +0300

    Multiplication Training - initial commit

---
+++ MultiplicationTraining/MultiplicationTraining/MultiplicationTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTrainingApp.swift
+//  MultiplicationTraining
+//
+//  Created by Igor Chernyshov on 09.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

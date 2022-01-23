d3dea5d98d5ef284da491e4efa075cf5fe777ab4
IgorChernyshov <ichernyshov@bk.ru>
2022-01-23 19:32:42 +0300

    Habit Tracker - initial commit

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Igor Chernyshov on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

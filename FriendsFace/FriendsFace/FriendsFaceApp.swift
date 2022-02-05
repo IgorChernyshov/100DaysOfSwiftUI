c2933242a5b281bdcac9de5737df449f483c5070
IgorChernyshov <ichernyshov@bk.ru>
2022-02-05 15:50:38 +0300

    Friends Face - initial commit

---
+++ FriendsFace/FriendsFace/FriendsFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFaceApp.swift
+//  FriendsFace
+//
+//  Created by Igor Chernyshov on 05.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

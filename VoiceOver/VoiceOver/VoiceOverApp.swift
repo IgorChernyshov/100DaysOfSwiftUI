12386cd88f2129cc1b58a44f66992cb5a9058a7e
IgorChernyshov <ichernyshov@bk.ru>
2022-03-04 20:21:06 +0300

    Voice Over - initial commit

---
+++ VoiceOver/VoiceOver/VoiceOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceOverApp.swift
+//  VoiceOver
+//
+//  Created by Igor Chernyshov on 04.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct VoiceOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

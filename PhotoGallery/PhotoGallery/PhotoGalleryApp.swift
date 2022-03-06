d56b9e1b5e62e1ed84ca64ca6e9184defec8da1a
IgorChernyshov <ichernyshov@bk.ru>
2022-03-06 09:26:59 +0300

    Photo Gallery - initial commit

---
+++ PhotoGallery/PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Igor Chernyshov on 06.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

a636ecb3662f5c9feb8a8a5bbac339efc759ebe1
IgorChernyshov <ichernyshov@bk.ru>
2022-03-08 11:21:35 +0300

    Hot Prospects - initial commit

---
+++ Hot Prospects/Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Igor Chernyshov on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

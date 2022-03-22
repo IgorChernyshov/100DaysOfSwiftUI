4f264689f8bda3974dd1beb657c65c8c9d4f419f
IgorChernyshov <ichernyshov@bk.ru>
2022-03-22 13:40:24 +0300

    Layout And Geometry - initial commit

---
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Igor Chernyshov on 20.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+			.padding()
+			.background(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Igor Chernyshov on 20.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

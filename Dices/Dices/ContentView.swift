f857cc3876fac195053dbea44b8900814f131862
IgorChernyshov <ichernyshov@bk.ru>
2022-03-26 11:16:25 +0300

    Dices - initial commit

---
+++ Dices/Dices/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dices
+//
+//  Created by Igor Chernyshov on 26.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dices/Dices/DicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicesApp.swift
+//  Dices
+//
+//  Created by Igor Chernyshov on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct DicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
